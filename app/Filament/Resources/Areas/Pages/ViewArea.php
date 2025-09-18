c12062df82291985b4b0a45c268e3b2f7cacd121
contrerasuriel57 <contrerasuriel57@gmail.com>
2025-09-18 14:47:25 -0600

    Cambio de color boton

--- app/Filament/Resources/Areas/Pages/ViewArea.php
+++ app/Filament/Resources/Areas/Pages/ViewArea.php
@@ -13,7 +13,8 @@ class ViewArea extends ViewRecord
     protected function getHeaderActions(): array
     {
         return [
-            EditAction::make(),
+            EditAction::make()
+            ->color('warning'),
         ];
     }
 }
